be5177d193d7429fecf245d8a57600eedcfa06a5
_pragramist <122969603+pragramist-07@users.noreply.github.com>
2023-10-08 14:40:42 +0300

    Update Program.cs
    Add commit

--- Registr/Program.cs
+++ Registr/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
