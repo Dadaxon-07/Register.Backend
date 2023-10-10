7c944209a12ab3b2dd9fd19c18f8e79f68f5b99b
ASUS <ASUS@HOME-PC>
2023-10-10 18:07:04 +0300

    Add migration

--- Registr/DataLayer/RegistrDBContext.cs
+++ Registr/DataLayer/RegistrDBContext.cs
@@ -10,7 +10,7 @@ namespace Register.Backend.DataLayer
         {
             
         }
-        public DbSet<User>MyProperty { get; set; }
+        public DbSet<User> Users { get; set; }
 
         
 
---
+++ Registr/Migrations/20231010150459_Registrmigiration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Register.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Registrmigiration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    EmailAddress = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
